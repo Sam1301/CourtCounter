c231d431dd7000bf56b0c9b5940907cd56a96136
Sam <saumya.bhatnagar.sb@gmail.com>
2016-07-05 13:15:08 +0530

    implemented code reviews

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // global variables to store scores of both teams
     int scoreBoardA;
     int scoreBoardB;
 
@@ -14,49 +15,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    public void updateTeamA(View v){
+    /**
+     * Function to calculate score for both teams whenever a button is pressed
+     * and update the screen accordingly.
+     *
+     * @param v ,where v is the view clicked
+     */
+    public void updatePoints(View v) {
 
-        TextView scoreA = (TextView) findViewById(R.id.scoreA);
-        switch(v.getId()){
-            case R.id.teamA1:
+        // to find if its a 1, 2 or 3 pointer
+        switch (v.getId()) {
+            case R.id.team_a_1:
                 scoreBoardA += 1;
                 break;
-            case R.id.teamA2:
+            case R.id.team_a_2:
                 scoreBoardA += 2;
                 break;
-            case R.id.teamA3:
+            case R.id.team_a_3:
                 scoreBoardA += 3;
                 break;
-        }
-        scoreA.setText(scoreBoardA + "");
-    }
-    public void updateTeamB(View v){
-
-        TextView scoreB = (TextView) findViewById(R.id.scoreB);
-        switch(v.getId()){
-            case R.id.teamB1:
+            case R.id.team_b_1:
                 scoreBoardB += 1;
                 break;
-            case R.id.teamB2:
+            case R.id.team_b_2:
                 scoreBoardB += 2;
                 break;
-            case R.id.teamB3:
+            case R.id.team_b_3:
                 scoreBoardB += 3;
                 break;
         }
-        scoreB.setText(scoreBoardB + "");
+
+        // display updated scores
+        displayScores();
     }
 
-    public void reset(View v){
-        TextView scoreA = (TextView) findViewById(R.id.scoreA);
-        TextView scoreB = (TextView) findViewById(R.id.scoreB);
+
+    /**
+     * Function to reset scores of both teams when
+     * reset button is clicked.
+     *
+     * @param v ,where v is the view clicked
+     */
+    public void reset(View v) {
+
+        // reset scores to zero for both teams
         scoreBoardA = 0;
         scoreBoardB = 0;
-        scoreA.setText(scoreBoardA + "");
-        scoreB.setText(scoreBoardB + "");
+
+    }
+
+    /**
+     * Function to display current scores for both teams on the screen.
+     */
+    public void displayScores() {
+        TextView scoreA = (TextView) findViewById(R.id.scoreA);
+        TextView scoreB = (TextView) findViewById(R.id.scoreB);
+
+        // display updated scores for both teams
+        scoreA.setText(String.format(getResources().getString(R.string.display_score), scoreBoardA));
+        scoreB.setText(String.format(getResources().getString(R.string.display_score), scoreBoardB));
     }
 
 }
